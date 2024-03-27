dfad7cad9a4d9847029ee7c1608ecb3c8581f237
mcrgit530 <164854745+mcrgit530@users.noreply.github.com>
2024-03-27 16:33:12 +0530

    Update app.js

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const findOrCreate = require('mongoose-findorcreate');
 
 mongoose.set('debug',true)
 const app = express();
+app.use(express.static(path.join(__dirname, "public")));
 app.set('view engine','ejs');
 app.use(bodyParser.urlencoded({
   extended: true
