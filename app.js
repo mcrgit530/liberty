13ce41b15a5c17250ed6ebf8c74bff56403843fe
mcrgit530 <164854745+mcrgit530@users.noreply.github.com>
2024-04-02 13:24:05 +0530

    Update app.js

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(session({
   secret: 'keyboard cat',
   resave: false,
   saveUninitialized: true,
-  cookie: { secure: true }
+  cookie: { secure: false }
 }));
 
 mongoose.connect(process.env.DBHOST);
